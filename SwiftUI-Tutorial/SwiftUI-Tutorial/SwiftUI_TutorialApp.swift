556d02fb98eb2b17ca5fb1c1fb4a3c226a1ee286
Kyle Ferrigan <kyle@ferrigan.dev>
2022-10-25 14:39:58 -0500

    Add files via upload

---
+++ SwiftUI-Tutorial/SwiftUI-Tutorial/SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialApp.swift
+//  SwiftUI-Tutorial
+//
+//  Created by Kyle Ferrigan on 9/26/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
